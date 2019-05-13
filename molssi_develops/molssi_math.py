fc3352c075c297c30322d7595c390fc4ed14052c
Emilie Guidez <emilie.guidez@ucdenver.edu>
2019-05-13 16:21:18 -0600

    added molssi math test

--- molssi_develops/molssi_math.py
+++ molssi_develops/molssi_math.py
@@ -6,6 +6,31 @@ Handles the primary functions
 """
 
 
+def mean(my_list):
+    """
+    This function calculates the mean of a list
+  
+    Parameters
+    ----------
+    my_list: list
+        The list of numbers that we want to average
+
+    Returns
+    -------
+    mean_list : float
+        The mean of the list
+
+    Examples
+    -------
+    >>> mean ([1,2,3,4,5])
+    3.0
+    """
+    if not isinstance(my_list, list):
+        raise TypeError("Mean: {} is not a list".format(my_list))
+
+    return sum(my_list) / len(my_list)
+
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
